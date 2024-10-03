291b4207c873728db7507b7c2a84c66de67a1154
LeyyAndrew <leiiiandrewwwcamara@gmail.com>
2024-10-03 09:06:53 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout << "Our Lady Of Lourdes College Foundation" <<endl;
+	cout << "College Of Computer Studies" <<endl;
+	cout << "BSIT-1A" <<endl;
+	cout << "Camara,Lei Andrew F." <<endl;
+	cout << "Malatap,Labo,Camarines Norte" <<endl;
+	return 0;
+}
